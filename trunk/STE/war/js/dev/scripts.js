79967e689fea7125c0b42993f22ca7d5b45cdca0
david.martin.beltran.contractor@bbva.com <david.martin.beltran.contractor@bbva.com@0689df1d-961a-5ecd-5580-1b144a181d79>
2014-11-18 12:34:46 +0000

    correciones incidencias y alcance

--- trunk/STE/war/js/dev/scripts.js
+++ trunk/STE/war/js/dev/scripts.js
@@ -456,7 +456,7 @@ $(function() {
 						$('.form-holder').height($('.form-holder').height()+35);
 					}
 					$form.find('.form-container').find('div:not(#message_div)').hide(0);
-					$form.find('#span_message').html('El soporte ha sido creado de forma correcta.<br/>En breve volvemos a la p&aacute;gina.');
+					$form.find('#span_message').html('El cliente ha sido creado de forma correcta.<br/>En breve volvemos a la p&aacute;gina.');
 					$('#message_div').css('display','block').removeClass("error").addClass("success");;
 
 					setTimeout(function() { 
@@ -685,7 +685,7 @@ $(function() {
 };
 
 ;var normalize = (function() {
-	var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",
+	var from = "ÃƒÃ€Ã�Ã„Ã‚ÃˆÃ‰Ã‹ÃŠÃŒÃ�Ã�ÃŽÃ’Ã“Ã–Ã”Ã™ÃšÃœÃ›Ã£Ã Ã¡Ã¤Ã¢Ã¨Ã©Ã«ÃªÃ¬Ã­Ã¯Ã®Ã²Ã³Ã¶Ã´Ã¹ÃºÃ¼Ã»Ã‘Ã±Ã‡Ã§",
 	  to   = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
 	  mapping = {};
 
@@ -996,23 +996,23 @@ $(function() {
 		required: "Este campo es obligatorio.",
 		remote: "Por favor, rellena este campo.",
 		email: "Por favor, escribe una direcci&oacuten de correo v&aacutelida.(Terminada en @bbva.com)",
-		url: "Por favor, escribe una URL válida.",
-		date: "Por favor, escribe una fecha válida.",
-		dateISO: "Por favor, escribe una fecha (ISO) válida.",
+		url: "Por favor, escribe una URL vÃ¡lida.",
+		date: "Por favor, escribe una fecha vÃ¡lida.",
+		dateISO: "Por favor, escribe una fecha (ISO) vÃ¡lida.",
 		number: "Por favor, escribe un n&uacutemero v&aacutelido.",
-		digits: "Por favor, escribe s&oacutelo dígitos.",
-		creditcard: "Por favor, escribe un número de tarjeta válido.",
+		digits: "Por favor, escribe s&oacutelo dÃ­gitos.",
+		creditcard: "Por favor, escribe un nÃºmero de tarjeta vÃ¡lido.",
 		equalTo: "Por favor, escribe el mismo valor de nuevo.",
-		extension: "Por favor, escribe un valor con una extensión aceptada.",
-		maxlength: $.validator.format("Por favor, no escribas más de {0} caracteres."),
+		extension: "Por favor, escribe un valor con una extensiÃ³n aceptada.",
+		maxlength: $.validator.format("Por favor, no escribas mÃ¡s de {0} caracteres."),
 		minlength: $.validator.format("Por favor, no escribas menos de {0} caracteres."),
 		rangelength: $.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
 		range: $.validator.format("Por favor, escribe un valor entre {0} y {1}."),
 		max: $.validator.format("Por favor, escribe un valor menor o igual a {0}."),
 		min: $.validator.format("Por favor, escribe un valor mayor o igual a {0}."),
-		nifES: "Por favor, escribe un NIF válido.",
-		nieES: "Por favor, escribe un NIE válido.",
-		cifES: "Por favor, escribe un CIF válido."
+		nifES: "Por favor, escribe un NIF vÃ¡lido.",
+		nieES: "Por favor, escribe un NIE vÃ¡lido.",
+		cifES: "Por favor, escribe un CIF vÃ¡lido."
 	});
 
 	
